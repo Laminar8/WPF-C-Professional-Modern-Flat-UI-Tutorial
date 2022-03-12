0ff7fcdf5bd526d986d0a24f852bc90399ae447e
laminar8 <oliver_mique@aol.com>
2022-03-12 23:02:25 +0900

    RelayCommand

---
+++ ModernDesign/Core/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernDesign.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManger.RequerySuggested += value; }
+            remove { CommandManger.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, )
+    }
+}
