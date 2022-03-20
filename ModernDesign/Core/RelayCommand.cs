53b1f2669518dadc796c05ce150f5202838f00d9
laminar8 <oliver_mique@aol.com>
2022-03-21 00:00:36 +0900

    Applied Button Sytle

--- ModernDesign/Core/RelayCommand.cs
+++ ModernDesign/Core/RelayCommand.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace ModernDesign.Core
 {
     internal class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
 
         public event EventHandler CanExecuteChanged
         {
@@ -18,6 +14,21 @@ namespace ModernDesign.Core
             remove { CommandManger.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, )
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
